16e149b88d83512b871cc511a381a55fd3f5130c
João Carvalho <jaocarvalho.vlad@gmail.com>
2026-01-09 15:33:41 -0300

    first commit

---
+++ src/main/java/com/oakdev/notificacao/business/dto/TarefasDTO.java
@@ -0,0 +1,30 @@
+package com.oakdev.notificacao_email.business.dto;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.oakdev.notificacao_email.infrastructure.enums.StatusNotificacaoEnum;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Setter
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class TarefasDTO {
+    private String id;
+
+    private String nomeTarefa;
+    private String descricao;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy HH:mm:ss" )
+
+    private LocalDateTime dataCriacao;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy HH:mm:ss" )
+    private LocalDateTime dataEvento;
+    private String emailUsuario;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy HH:mm:ss" )
+
+    private LocalDateTime dataAlteracao;
+    private StatusNotificacaoEnum statusNotificacaoEnum;
+}
---
+++ src/main/java/com/oakdev/notificacao/infrastructure/enums/StatusNotificacaoEnum.java
@@ -0,0 +1,5 @@
+package com.oakdev.notificacao_email.infrastructure.enums;
+
+public enum StatusNotificacaoEnum {
+    PENDENTE, NOTIFICADO,  CANCELADO;
+}
